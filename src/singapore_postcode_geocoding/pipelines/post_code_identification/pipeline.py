9b906bd7162002433f17de0cc8b3181a467670c7
ejwillemse <ejwillemse@gmail.com>
2025-01-30 20:11:26 +0000

    add dummy postcode identification pipeline

---
+++ src/singapore_postcode_geocoding/pipelines/post_code_identification/pipeline.py
@@ -0,0 +1,72 @@
+# from kedro.pipeline import Pipeline, node
+
+# from .nodes import (
+#     enrich_open_postcode,
+#     extend_onemap,
+#     format_onemap,
+#     format_opendata,
+#     format_postcodebase,
+#     postcode_full_type_conversion,
+#     return_postcode_master_list,
+# )
+
+
+# def create_pipeline(**kwargs) -> Pipeline:
+#     return Pipeline(
+#         [
+#             node(
+#                 func=format_onemap,
+#                 inputs=["one_map_scrape", "params:postcode_validation"],
+#                 outputs="formatted_onemap",
+#                 name="format_onemap",
+#             ),
+#             node(
+#                 func=format_opendata,
+#                 inputs=["open_data_postal_code", "params:postcode_validation"],
+#                 outputs="formatted_opendata",
+#                 name="format_opendata",
+#             ),
+#             node(
+#                 func=format_postcodebase,
+#                 inputs=["sg_postcode_based_via_getdata", "params:postcode_validation"],
+#                 outputs="formatted_postcodebase",
+#                 name="format_postcodebase",
+#             ),
+#             node(
+#                 func=enrich_open_postcode,
+#                 inputs=[
+#                     "formatted_opendata",
+#                     "formatted_postcodebase",
+#                     "params:postcode_validation",
+#                 ],
+#                 outputs="enriched_opendata",
+#                 name="enrich_opendata",
+#             ),
+#             node(
+#                 func=extend_onemap,
+#                 inputs=[
+#                     "formatted_onemap",
+#                     "enriched_opendata",
+#                     "params:postcode_validation",
+#                 ],
+#                 outputs="extended_onemap",
+#                 name="extend_onemap",
+#             ),
+#             node(
+#                 func=postcode_full_type_conversion,
+#                 inputs=["extended_onemap", "params:postcode_validation"],
+#                 outputs="singapore_postcodes_geocoded",
+#                 name="convert_types",
+#             ),
+#             node(
+#                 func=return_postcode_master_list,
+#                 inputs=["singapore_postcodes_geocoded", "params:postcode_validation"],
+#                 outputs="singapore_postcodes_masterlist",
+#                 name="return_postcode_master_list",
+#             ),
+#         ]
+#     )
+
+
+def create_pipeline():
+    pass
